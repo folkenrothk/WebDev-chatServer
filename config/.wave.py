12bb0f1a5ad943f13b82be426db8c4c916ca7492
dluman <dluman@allegheny.edu>
2022-05-14 13:01:02 -0400

    Enabling silent mode on automated game player

--- config/.wave.py
+++ config/.wave.py
@@ -3,31 +3,30 @@ import json
 import requests
 import os
 
+
 def main():
 
-  pages_api = os.getenv("PAGES_URL")
-  wave_api = os.getenv("WAVE_API")
+    pages_api = os.getenv("PAGES_URL")
+    wave_api = os.getenv("WAVE_API")
 
-  data = json.loads(pages_api)
-  endpoint = data["html_url"]
-  page = sys.argv[1]
+    data = json.loads(pages_api)
+    endpoint = data["html_url"]
+    page = sys.argv[1]
 
-  validator = requests.get(
-    "https://wave.webaim.org/api/request",
-    params = {
-      "key":wave_api,
-      "url":f"{endpoint}{page}"
-    }
-  )
+    validator = requests.get(
+        "https://wave.webaim.org/api/request",
+        params={"key": wave_api, "url": f"{endpoint}{page}"},
+    )
 
-  response = json.loads(validator.text)
+    response = json.loads(validator.text)
 
-  contrast_count = int(response['categories']['contrast']['count'])
-  error_count = int(response['categories']['error']['count])
+    contrast_count = int(response["categories"]["contrast"]["count"])
+    error_count = int(response["categories"]["error"]["count"])
 
-  if contrast_count + error_count < 1:
-    sys.exit(0)
-  sys.exit(1)
+    if contrast_count + error_count < 1:
+        sys.exit(0)
+    sys.exit(1)
+
 
 if __name__ == "__main__":
-  main()
+    main()
